d0dd55091198b906cc7273850563ed2081b722cd
Rupesh2010 <nclexgirl8@gmail.com>
2019-05-29 22:10:43 -0400

    iam

---
+++ east/iam.tf
@@ -0,0 +1,13 @@
+data "aws_iam_policy_document" "default" {
+  statement {
+  
+    actions = [
+      "s3:ListAllMyBuckets",
+      "s3:GetBucketLocation",
+    ]
+
+    resources = [
+      "arn:aws:s3:::*",
+    ]
+  }
+}
