6610a53e64d1edeba66f865cb68b96c7e93e227d
Rupesh2010 <nclexgirl8@gmail.com>
2019-05-29 22:12:04 -0400

    iam

--- east/iam.tf
+++ east/iam.tf
@@ -11,3 +11,8 @@ data "aws_iam_policy_document" "default" {
     ]
   }
 }
+
+resource "aws_iam_policy" "default" {
+  name   = "ec2_policy"
+  policy = "${data.aws_iam_policy_document.default.json}"
+}
